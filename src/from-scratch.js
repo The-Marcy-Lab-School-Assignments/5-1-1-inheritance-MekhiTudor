f5503d59667a2a54f7769e69dd4eab04afd6805c
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-07-24 17:20:56 -0400

    done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,19 +1,38 @@
 class Quadrilateral {
-
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+    this.side4 = side4;
+  }
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3 + this.side4;
+  }
 }
 
-class Rectangle {
-
+class Rectangle extends Quadrilateral {
+  constructor(long, short) {
+    super(long, short, long, short);
+  }
+  getArea() {
+    return this.side1 * this.side2;
+  }
 }
 
-class Square {
+class Square extends Rectangle {
+  constructor(length) {
+    super(length, length);
+    this.length = length;
+  }
 
+  getDiagonal() {
+    return Math.sqrt(2 * this.getArea());
+  }
 }
-
+const square = new Square(10);
+square.getDiagonal();
 /* Be creative with this one! */
-class Person {
-
-}
+class Person {}
 
 module.exports = {
   Quadrilateral,
